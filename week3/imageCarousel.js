71ba05ab3938635b8516a1bde35f806f3811ab7c
ZeeshanAhmadHW <za2044@hw.ac.uk>
2023-10-17 14:21:05 +0400

    Update imageCarousel.js

--- week3/imageCarousel.js
+++ week3/imageCarousel.js
@@ -4,13 +4,18 @@ var interval = 1000;
 images = ["images/1.jpeg","images/2.jpeg","images/3.jpeg","images/4.jpeg",
           "images/5.jpeg","images/6.jpeg","images/7.jpeg","images/8.jpeg"];
 
+function carouselshift(){
+    document.querySelector('[name="slide"]').src = images[current];
+    if (current === 7){
+        current = 0;
+    }else{
+        current = current + 1;
+    }
+    
+}
+
+imageloop = setInterval(carouselshift,interval);
+
 function slide(){
-    imageloop = setInterval(function(){
-        document.querySelector('[name="slide"]').src = images[current];
-        if (current === 7){
-            current = 0;
-        }else{
-            current = current + 1;
-        }
-    },interval)
+    imageloop;
 }
