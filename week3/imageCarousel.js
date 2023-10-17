76f8f7940fa30f59c4a6412f93d65d01a6885c8b
ZeeshanAhmadHW <za2044@hw.ac.uk>
2023-10-17 14:00:42 +0400

    simplification of carousel
    simplification in js

--- week3/imageCarousel.js
+++ week3/imageCarousel.js
@@ -5,16 +5,12 @@ images = ["images/1.jpeg","images/2.jpeg","images/3.jpeg","images/4.jpeg",
           "images/5.jpeg","images/6.jpeg","images/7.jpeg","images/8.jpeg"];
 
 function slide(){
-    if (current === 0 || current === 8){
-        current = 1;
-    }else{
-        current = current + 1;
-    }
-    document.querySelector('[name="slide"]').src = images[current]
-}
-
-imageloop = setInterval(slide,interval);
-
-function carousel(){
-    imageloop;
+    imageloop = setInterval(function(){
+        if (current === 0 || current === 8){
+            current = 1;
+        }else{
+            current = current + 1;
+        }
+        document.querySelector('[name="slide"]').src = images[current]
+    },interval)
 }
