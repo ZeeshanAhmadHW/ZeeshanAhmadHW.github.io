66d1b232b17b28e75a98104688c452350b322d2b
ZeeshanAhmadHW <za2044@hw.ac.uk>
2023-11-05 15:58:57 +0400

    first attempt at weather app

---
+++ week4/weatherApp/app.js
@@ -0,0 +1,24 @@
+var btn = document.getElementById("btn");
+var info = document.getElementById("weather-info");
+var apikey = "addlater"
+
+btn.addEventListener("click",function(){
+    var input = document.getElementById("cityInput").innerHTML;
+
+    if (input === ""){
+        window.alert("Enter a city name");
+    }else{
+        var Request = new XMLHttpRequest();
+
+        Request.open('GET',("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apikey + "&units=metric"));
+        Request.onload = function() {
+            var data = JSON.parse(Request.responseText);
+            
+            info.insertBefore("<hr>",info.firstChild);
+            info.insertBefore(("The temperature is " + data.main.temp + "°C with a wind speed of " + data.wind.speed + "m/s."),info.firstChild);
+            info.insertBefore(("The weather in " + input + " is " + data.weather[0].description + "."),info.firstChild);
+            input.innerHTML = "";
+        };
+        Request.send();
+    }
+})
