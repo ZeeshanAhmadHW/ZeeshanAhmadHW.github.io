554d6f8ea3f9414a89d9436c13ccc13aa0f4ffa8
ZeeshanAhmadHW <za2044@hw.ac.uk>
2023-11-09 12:49:04 +0400

    (hopefully) final commit commit for lab4

--- week4/weatherApp/app.js
+++ week4/weatherApp/app.js
@@ -8,11 +8,11 @@ btn.addEventListener("click",function(){
     if (input === ""){
         window.alert("Enter a city name");
     }else{
-        var Request = new XMLHttpRequest();
+        var request = new XMLHttpRequest();
 
-        Request.open('GET',("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apikey + "&units=metric"));
-        Request.onload = function() {
-            var data = JSON.parse(Request.responseText);
+        request.open('GET',("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apikey + "&units=metric"));
+        request.onload = function() {
+            var data = JSON.parse(request.responseText);
             var line1 = document.createElement("p");
             var line2 = document.createElement("p");
 
@@ -20,7 +20,11 @@ btn.addEventListener("click",function(){
             line2.innerHTML = "The temperature is " + data.main.temp + "°C with a wind speed of " + data.wind.speed + "m/s.<hr>";
             info.insertBefore(line2,info.firstChild);
             info.insertBefore(line1,info.firstChild);
+            input.innerhtml = "";
         };
-        Request.send();
+        request.onerror = function() {
+            window.alert("Error " + request.status + ": " + request.statusText);
+        }
+        request.send();
     }
 })
